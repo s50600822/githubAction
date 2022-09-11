516a994e5350b9231b95d288fc615348346c9e11
HoaPhan <s50600822@gmail.com>
2022-09-11 15:32:03 +1000

    headless

--- sel.py
+++ sel.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.keys import Keys
 
 
 ops = webdriver.FirefoxOptions()
-ops.set_headless()
-browser = webdriver.Firefox(firefox_options=ops)
+ops.headless=True
+browser = webdriver.Firefox(options=ops)
 
 browser.get('https://hoaphan.net')
 print(browser.find_element(By.TAG_NAME, "body").text)
